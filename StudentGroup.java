e8d66f85c6853ab6f41e06540ee9776ac0f5de46
griet1 <31843637+griet1@users.noreply.github.com>
2017-09-12 13:43:49 +0530

    compilation error

---
+++ StudentGroup.java
@@ -0,0 +1,14 @@
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+
+	 private string students[]=new student[100];
+
+	 
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+                }
+             public Student[] getStudents() {
+		private string student[]=new student[100];
+               
+                                }
